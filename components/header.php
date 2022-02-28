3a90c722c8d6918045ea7f876159d72c84ebc2c3
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-02-28 17:00:52 +0100

    Display of bots on frontpage

---
+++ components/header.php
@@ -0,0 +1,30 @@
+<header class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="../assest/img/logo.svg" alt="logo">
+            </div>
+            <div class="col-lg-8">
+                <nav class="navbar">
+                    <ul class="nav w-100 nav-fill">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Robots</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Kalender</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Livestream</a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link text-danger" href="#">Login / Registeren</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- pages/frontpage.php
+++ pages/frontpage.php
@@ -1,9 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include_once('../components/head.html')?>
-    <title>Voorpagina</title>
+<?php include_once('../components/head.html') ?>
+<title>Voorpagina</title>
 </head>
+
 <body>
-    
+    <section id="header">
+        <?php include_once('../components/header.php') ?>`
+    </section>
+    <section id="content">
+        <div class="container">
+            <div class="row my-4">
+                <div class="col-6">
+                    <h1>Welkom op Battlebots</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero distinctio, voluptate tempora enim expedita repellat nesciunt vero, impedit delectus non vel dolorum? Quidem repudiandae maiores, facilis maxime ab natus!
+                    </p>
+                </div>
+                <div class="col-lg-6">
+                    <img src="../assets/img/battlebots.png" alt="Battlebots">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="bots">
+        <div class="container">
+            <div class="row my-4">
+                <div class="col-12 text-center">
+                    <h3>Ontmoet de Bots</h3>
+                </div>
+                <div class="col-lg-12">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
