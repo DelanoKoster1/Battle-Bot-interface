6afb4237a049dcaa0181819ba67cc9a072889609
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-03-02 16:31:07 +0100

    Made points page

---
+++ pages/pointspage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("../components/head.html"); ?>
+<link rel="stylesheet" href="../assets/css/pointspage.css">
+<title>Punten Pagina</title>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Scores</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="w-25" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                    </div>
+                    <div class="col-12">
+                        <p>Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+
+    </div>
+</body>
+
+</html>
