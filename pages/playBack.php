6420ac8116fc37f1e438088ab1ba1dae93499354
robin <robenjay@hotmail.nl>
2022-03-03 10:38:30 +0100

    small update playback page

---
+++ pages/playBack.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('../components/head.html')?>
+    <?php include_once('../components/header.php')?>
+    <link rel="stylesheet" href="../assets/css/playback.css">
+    <title>playBack pagina</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <video class="playVideo" width="990" height="500" controls>
+
+                </video>
+            </div>
+            <div class="col-md-2">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
