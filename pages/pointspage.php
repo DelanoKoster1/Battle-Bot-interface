58cee6dec73cc6438a9d7c67decc9d4a6f22b641
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-03-03 11:09:15 +0100

    pointspage completed except header, footer and font

--- pages/pointspage.php
+++ pages/pointspage.php
@@ -1,35 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once("../components/head.html"); ?>
-<link rel="stylesheet" href="../assets/css/pointspage.css">
 <title>Punten Pagina</title>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h1>Scores</h1>
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center pt-3">
+            <h1>Scores</h1>
         </div>
-        <div class="row">
-            <div class="col-2"></div>
-            <div class="col-8">
-                <div class="row">
-                    <div class="col-4">
-                        <img class="w-25" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
-                    </div>
+    </div>
+    <div class="row pt-3">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-10">
                     <div class="col-12">
-                        <p>Bot name (INF1X)</p>
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
                     </div>
                     <div class="col-12 bg-primary text-white">
-                        <p class="ps-4">2,019 PT</p>
+                        <p class="p-2 ps-4">2,019 PT</p>
                     </div>
                 </div>
             </div>
-            <div class="col-2"></div>
         </div>
-
+        <div class="col-2"></div>
     </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-10">
+                    <div class="col-12">
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="p-2 ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-10">
+                    <div class="col-12">
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="p-2 ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-10">
+                    <div class="col-12">
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="p-2 ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-10">
+                    <div class="col-12">
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="p-2 ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+</div>
+
 </body>
 
 </html>
