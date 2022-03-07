75fcacce9509284939433d9a05aac3f5644bebda
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-03-07 14:29:07 +0100

    fixed indenting calendarpage

--- pages/calendarpage.php
+++ pages/calendarpage.php
@@ -5,59 +5,56 @@
     <title>Calendar</title>
 </head>
 <body>
-        <header>
-            <?php include_once('../components/header.php') ?>
-        </header>
-
-        <main id='events'>
-            <div class='containter py-4'>
-                <div class="row">
-                    <div class="col-12 mb-2 text-center">
-                        <h3>Komende evenementen</h3>
-                    </div>
-                    <div class="col-lg-12 d-flex justify-content-center">
-                        <div class="card mx-3 event">
-                            <div class="calendarbox d-flex justify-content-center align-items-center">
-                                <div>
-                                    <span class="calendarDay d-block">25</span>
-                                    <span class="calendarMonth">Maart</span>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title text-center">Test dag</h6>
-                                <div class="text-center">
-                                    <span>The testing date for the official event</span>
-                                </div>
-                                <div class="time text-center">
-                                    <span>08:30 - 12:15</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mx-3 event">
-                            <div class="calendarbox d-flex justify-content-center align-items-center">
-                                <div>
-                                    <span class="calendarDay d-block">14</span>
-                                    <span class="calendarMonth">April</span>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title text-center">Race dag</h6>
-                                <div class="text-center">
-                                    <span>Official BattleBots event date</span>
-                                </div>
-                                <div class="time text-center text-bottom">
-                                    <span>08:30 - 12:15</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
+    <header>
+        <?php include_once('../components/header.php') ?>
+    </header>
+    <main id='events'>
+        <div class='containter py-4'>
+            <div class="row">
+                <div class="col-12 mb-2 text-center">
+                    <h3>Komende evenementen</h3>
                 </div>
+                <div class="col-lg-12 d-flex justify-content-center">
+                    <div class="card mx-3 event">
+                        <div class="calendarbox d-flex justify-content-center align-items-center">
+                            <div>
+                                <span class="calendarDay d-block">25</span>
+                                <span class="calendarMonth">Maart</span>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="card-title text-center">Test dag</h6>
+                            <div class="text-center">
+                                <span>The testing date for the official event</span>
+                            </div>
+                            <div class="time text-center">
+                                <span>08:30 - 12:15</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mx-3 event">
+                        <div class="calendarbox d-flex justify-content-center align-items-center">
+                            <div>
+                                <span class="calendarDay d-block">14</span>
+                                <span class="calendarMonth">April</span>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="card-title text-center">Race dag</h6>
+                            <div class="text-center">
+                                <span>Official BattleBots event date</span>
+                            </div>
+                            <div class="time text-center text-bottom">
+                                <span>08:30 - 12:15</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>                 
             </div>
-        </main>
-
-        <footer>
-            <?php include_once('../components/footer.php') ?>
-        </footer>
+        </div>
+    </main>
+    <footer>
+        <?php include_once('../components/footer.php') ?>
+    </footer>
 </body>
 </html>
