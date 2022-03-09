fd6b40e035c5989c3936c1f9f0d4f484792113a5
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-03-09 17:33:51 +0100

    made beginning of new livestream page with design from mockups

--- pages/livestream.php
+++ pages/livestream.php
@@ -11,54 +11,75 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
 
-    <title>playBack pagina</title>
+    <title>Livestream - Battlebot</title>
 </head>
 
 <body>
-<section id="header">
-    <?php includeHeader('page'); ?>
-</section>
+    <section id="header">
+        <?php includeHeader('page'); ?>
+    </section>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-9">
-            <div class="embed-responsive embed-responsive-16by9">
-                <video class="playVideo" width="990" height="550" controls>
-
-                </video>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-9">
+                <h1>Livestream</h1>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="chatBox">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 id="liveChat">Live Chat</h3>
-                    </div>
+            <div class="col-md-3">
+                <h1>Chat</h1>
+            </div>
+            <div class="col-md-9">
+                <div class="ratio ratio-16x9">
+                    <iframe id="ytplayer" type="text/html" src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"></iframe>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="chat">
-                            John Doe
-                            dit is een test.
+            </div>
+            <div class="col-md-3">
+                <div class="row h-100">
+                    <div class="col-12">
+                        <div class="card h-100 posistion-relative">
+                            <div class="card-body">
+                                <div class="chatLine my-2">
+                                    <span class="fw-bold">Username: </span>
+                                    <span class="message">Lorem ipsum dolor sit </span>
+                                </div>
+                                <div class="chatLine my-2">
+                                    <span class="fw-bold">Username: </span>
+                                    <span class="message">Lorem ipsum dolor sit </span>
+                                </div>
+                                <div class="chatLine my-2">
+                                    <span class="fw-bold">Username: </span>
+                                    <span class="message">Lorem ipsum dolor sit </span>
+                                </div>
+                                <div class="chatLine my-2">
+                                    <span class="fw-bold">Username: </span>
+                                    <span class="message">Lorem ipsum dolor sit </span>
+                                </div>
+                                <div class="commandLine position-absolute bottom-0 start-0">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="input-group mb-3">
+                                                <input type="text" class="form-control" placeholder="Type uw bericht" aria-label="Type uw bericht" aria-describedby="button-addon2">
+                                                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><span class="material-icons align-middle">send</span></button>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="btn w-33 btn-success">1000</div>
+                                            <button class="btn text-right btn-success">Scorebord</button>
+                                            <button class="btn text-right btn-success">Stemmen</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="chatField">
-                            <form method="post" action="">
-                                <input type="text" name="chatMessage" id="chatMessage" placeholder="text here..."/>
-                            </form>
-                        </div>
-                    </div>
+
+
                 </div>
             </div>
         </div>
     </div>
-</div>
-<footer>
-    <?php include_once('../components/footer.php') ?>
-</footer>
+    <footer>
+        <?php include_once('../components/footer.php') ?>
+    </footer>
 </body>
 
 </html>
