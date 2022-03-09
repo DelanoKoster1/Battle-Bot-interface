d7ebddc0a71864a4058e76d474c429cf707282ff
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-03-09 17:18:10 +0100

    Made a start to robots.php

---
+++ pages/robots.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include_once('../components/head.html');
+    include_once('../functions/function.php');
+    ?>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/robots.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    <title>Bot Page</title>
+</head>
+
+<body>
+    <section id="header">
+        <?php includeHeader('page'); ?>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/bot.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/botsetting.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Bot Setting</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/botsetting.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Bot Setting</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/botsetting.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Bot Setting</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/botsetting.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Bot Setting</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/botsetting.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Bot Setting</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <h1>Teams</h1>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6 my-3">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 my-auto">
+                            <img src="../assets/img/person.svg" alt="Logo of a bot">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span>Person Name</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <footer>
+        <?php include_once('../components/footer.php') ?>
+    </footer>
+</body>
+
+</html>
