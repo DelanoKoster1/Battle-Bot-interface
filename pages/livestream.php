7af824124a7feea2ecd3cea7acbeb4a353b4f303
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-03-14 13:24:40 +0100

    Made an responsive livestream page and cleaned up the code in header and footer

--- pages/livestream.php
+++ pages/livestream.php
@@ -19,56 +19,65 @@
         <?php includeHeader('page'); ?>
     </section>
 
-    <div class="container my-5">
+    <div class="container-fluid my-5">
         <div class="row">
-            <div class="col-md-9">
-                <h1>Livestream</h1>
-            </div>
-            <div class="col-md-3">
-                <h1>Chat</h1>
-            </div>
-            <div class="col-md-9">
+            <div id="livestream" class="col-xl-9 col-lg-8 col-12">
                 <div class="ratio ratio-16x9">
                     <iframe id="ytplayer" type="text/html" src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"></iframe>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div id="chat" class="col-xl-3 col-lg-4 col-12 mt-4 mt-lg-0">
                 <div class="row h-100">
                     <div class="col-12">
-                        <div class="card h-100 posistion-relative">
-                            <div class="card-body">
-                                <div class="chatLine my-2">
-                                    <span class="fw-bold">Username: </span>
-                                    <span class="message">Lorem ipsum dolor sit </span>
+                        <div id="chatbox" class="chatbox h-100 collsapse show collapse-horizontal">
+                            <div class="card h-100 posistion-relative">
+                                <div class="card-header text-center">
+                                    <div data-bs-toggle="collapse" data-bs-target="#chatbox" aria-expanded="true" aria-controls="chatbox" class="btn btn-sm btn-outline-success float-start d-none d-lg-block">
+                                        <span class="material-icons align-middle">keyboard_arrow_right </span>
+                                    </div>
+                                    <span class="fw-bold">
+                                        Chat
+                                    </span>
                                 </div>
-                                <div class="chatLine my-2">
-                                    <span class="fw-bold">Username: </span>
-                                    <span class="message">Lorem ipsum dolor sit </span>
-                                </div>
-                                <div class="chatLine my-2">
-                                    <span class="fw-bold">Username: </span>
-                                    <span class="message">Lorem ipsum dolor sit </span>
-                                </div>
-                                <div class="chatLine my-2">
-                                    <span class="fw-bold">Username: </span>
-                                    <span class="message">Lorem ipsum dolor sit </span>
-                                </div>
-                                <div class="commandLine position-absolute bottom-0 start-0">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="input-group mb-3">
-                                                <input type="text" class="form-control" placeholder="Type uw bericht" aria-label="Type uw bericht" aria-describedby="button-addon2">
-                                                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><span class="material-icons align-middle">send</span></button>
-                                            </div>
+                                <div class="card-body">
+                                    <div class="chatmessage-container h-75">
+                                        <div class="chatLine my-2">
+                                            <span class="fw-bold">Username: </span>
+                                            <span class="message">Lorem ipsum dolor sit </span>
                                         </div>
-                                        <div class="col-12">
-                                            <div class="btn w-33 btn-success">1000</div>
-                                            <button class="btn text-right btn-success">Scorebord</button>
-                                            <button class="btn text-right btn-success">Stemmen</button>
+                                        <div class="chatLine my-2">
+                                            <span class="fw-bold">Username: </span>
+                                            <span class="message">Lorem ipsum dolor sit </span>
+                                        </div>
+                                        <div class="chatLine my-2">
+                                            <span class="fw-bold">Username: </span>
+                                            <span class="message">Lorem ipsum dolor sit </span>
+                                        </div>
+                                        <div class="chatLine my-2">
+                                            <span class="fw-bold">Username: </span>
+                                            <span class="message">Lorem ipsum dolor sit </span>
+                                        </div>
+                                    </div>
+                                    <div class="commandLine position-absolute bottom-0 start-0">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="input-group mb-3">
+                                                    <input type="text" class="form-control" placeholder="Type uw bericht" aria-label="Type uw bericht" aria-describedby="button-addon2">
+                                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><span class="material-icons align-middle">send</span></button>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 d-flex justify-content-between mb-3">
+                                                <div class="btn w-33 btn-success">1000</div>
+                                                <button class="btn text-right btn-success">Scorebord</button>
+                                                <button class="btn text-right btn-success">Stemmen</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div data-bs-toggle="collapse" id="openChatBtn" data-bs-target="#chatbox" aria-expanded="true" aria-controls="chatbox" class="btn d-none btn-sm btn-outline-success float-end">
+                            <span class="material-icons align-middle">keyboard_arrow_left</span>
                         </div>
                     </div>
 
@@ -80,6 +89,32 @@
     <footer>
         <?php include_once('../components/footer.php') ?>
     </footer>
+    <script>
+        let myCollapsible = document.getElementById('chatbox');
+        let chat = document.getElementById("chat");
+        let livestream = document.getElementById("livestream");
+        let openChatBtn = document.getElementById("openChatBtn");
+
+        myCollapsible.addEventListener('hidden.bs.collapse', function() {
+            chat.classList.remove("col-xl-3", "col-lg-4", "col-12");
+            chat.classList.add("col-md-1");
+
+            livestream.classList.remove("col-xl-9", "col-lg-8", "col-12");
+            livestream.classList.add("col-md-11");
+
+            openChatBtn.classList.remove("d-none");
+        });
+
+        myCollapsible.addEventListener('show.bs.collapse', function() {
+            chat.classList.add("col-xl-3", "col-lg-4", "col-12");
+            chat.classList.remove("col-md-1");
+
+            livestream.classList.add("col-xl-9", "col-lg-8", "col-12");
+            livestream.classList.remove("col-md-11");
+
+            openChatBtn.classList.add("d-none");
+        })
+    </script>
 </body>
 
 </html>
