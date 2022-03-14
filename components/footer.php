81f732a7036f88edbb380c74429d1d0d6f11c508
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-03-14 14:55:36 +0100

    fixed image

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,7 @@
         <div class="row text-center">
             <div class="col-md-6 col-12">
                 <h6>Dit evenement is gesponsord door:</h6>
-                <img class="footerIMG" src="assets/img/logo-nhl-stenden.png" alt="StendenLogo">
+                <img class="footerIMG" src="../assets/img/logo-nhl-stenden.png" alt="StendenLogo">
             </div>
             <div class="col-md-3 col-12">
                 <h3>Battlebot</h3>
