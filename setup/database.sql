a7df8b31a3c6d07de47f1d515413b69327aef5e4
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-03-16 16:13:58 +0100

    Update database.sql

--- setup/database.sql
+++ setup/database.sql
@@ -73,3 +73,19 @@ CREATE TABLE IF NOT EXISTS `team-event` (
 
     CONSTRAINT pk_team_event PRIMARY KEY (eventId, teamId)
     );
+
+INSERT INTO `role` (id, name) VALUES (1, "Default"), (2, "Admin");
+
+INSERT INTO `stats` (id) VALUES (1), (2), (3), (4), (5);
+
+INSERT INTO `specs` (id, board, interface) VALUES (1, "ESP32", "Arduino IDE"), (2, "ESP32", "Arduino IDE"), (3, "ESP32", "Arduino IDE"), (4, "ESP32", "Arduino IDE"), (5, "ESP32", "Arduino IDE");
+
+INSERT INTO `bot` (id, statsId, specsId, name, description, imagePath) VALUES (1, 1, 1, "Bot1", "Description Bot1", "image.png"), (2, 2, 2, "Bot2", "Description Bot2", "image.png"), (3, 3, 3, "Bot3", "Description Bot3", "image.png"), (4, 4, 4, "Bot4", "Description Bot4", "image.png"), (5, 5, 5, "Bot5", "Description Bot5", "image.png");
+
+INSERT INTO `team` (id, botId, name) VALUES (1, 1, "INF1A"), (2, 2, "INF1B"), (3, 3, "INF1C"), (4, 4, "INF1D"), (5, 5, "INF1E");
+
+INSERT INTO `account` (id, teamId, roleId, username, password, email) VALUES (1, 0, 1, "User", "test1", "user1@battlebot.nl"), (2, 0, 2, "Admin", "test2", "admin@battlebot.nl");
+
+INSERT INTO `event` (id, name, date, description) VALUES (1, "Main Event", "2022-04-14 08:30:00", "The main BattleBot Event!");
+
+INSERT INTO `team-event` (eventId, teamId) VALUES (1, 1);
