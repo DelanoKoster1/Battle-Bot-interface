3587b5e0468b06980291d2164d3e55c98da88705
ItzJoris03 <jorishummel03@gmail.com>
2022-03-16 11:24:20 +0100

    added custom font and fixed footer, not tested yet

--- components/footer.php
+++ components/footer.php
@@ -1,26 +1,29 @@
-<footer class="footer pt-4">
+<footer class="footer">
     <div class="container text-uppercase">
         <div class="row text-center">
-            <div class="col-md-6 col-12">
+            <div class="col-md-6 col-12 column">
                 <h6>Dit evenement is gesponsord door:</h6>
                 <div class="rounded-circle">
-                    <img class="footerIMG" src="<?=($_SESSION['sort'] == 'page') ? '../assets/img/logo-nhl-stenden.png' : 'assets/img/logo-nhl-stenden.png'?>" alt="StendenLogo">
+                    <img class="footerIMG" src="<?=($_SESSION['sort'] == 'page') ? '../assets/img/nhlstenden-logo.svg' : 'assets/img/nhlstenden-logo.svg'?>" alt="StendenLogo">
                 </div>
             </div>
-            <div class="col-md-3 col-12">
-                <h4>Battlebot</h4>
+            <div class="col-md-3 col-12 column">
+                <h6>Battlebot</h6>
                 <ul class="list-unstyled">
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? '../index.php' : 'index.php'?>">Voorpagina</a></li>
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? 'robots.php' : 'pages/robots.php'?>">Robots</a></li>
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? 'calendarpage.php' : 'pages/calendarpage.php'?>">Kalender</a></li>
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? 'livestream.php' : 'pages/livestream.php'?>">Livestream</a></li>
+                    <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? '../index.php' : 'index.php'?>">Voorpagina</a></li>
+                    <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'robots.php' : 'pages/robots.php'?>">Robots</a></li>
+                    <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'calendarpage.php' : 'pages/calendarpage.php'?>">Kalender</a></li>
+                    <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'livestream.php' : 'pages/livestream.php'?>">Livestream</a></li>
                 </ul>
             </div>
-            <div class="col-md-3 col-12">
-                <h4>Account</h4>
+            <div class="col-md-3 col-12 column">
+                <h6>Account</h6>
                 <ul class="list-unstyled">
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? 'profile.php' : 'pages/profile.php'?>">Profiel</a></li>
-                    <li><a class="text-white text-decoration-none" href="<?= ($_SESSION['sort'] == 'page') ? 'login.php' : 'pages/login.php'?>">Login/Registeren</a></li>
+                    <?php if(isset($_SESSION['username'])) { ?>
+                        <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'profile.php' : 'pages/profile.php'?>">Profiel</a></li>
+                    <?php } else { ?>
+                        <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'login.php' : 'pages/login.php'?>">Login/Registeren</a></li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
