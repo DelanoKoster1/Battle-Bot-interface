774b2a064d1656b61e1782954af5e98bf7b6a2c7
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-03-17 15:07:17 +0100

    Made part of pointspage responsive

--- pages/pointspage.php
+++ pages/pointspage.php
@@ -7,6 +7,7 @@
     ?>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="styelsheet" href="../assets/css/pointspage.css">
 
     <title>Punten Pagina</title>
 </head>
@@ -30,7 +31,26 @@
                 <div class="col-2 d-flex align-items-center">
                     <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
                 </div>
-                <div class="col-10">
+                <div class="col-12 col-md-10">
+                    <div class="col-10 col-md-12">
+                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
+                    </div>
+                    <div class="col-12 bg-primary text-white">
+                        <p class="p-2 ps-4">2,019 PT</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
+                </div>
+                <div class="col-12 col-md-10">
                     <div class="col-12">
                         <p class="mb-2 ps-2">Bot name (INF1X)</p>
                     </div>
@@ -49,7 +69,7 @@
                 <div class="col-2 d-flex align-items-center">
                     <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
                 </div>
-                <div class="col-10">
+                <div class="col-12 col-md-10">
                     <div class="col-12">
                         <p class="mb-2 ps-2">Bot name (INF1X)</p>
                     </div>
@@ -68,26 +88,7 @@
                 <div class="col-2 d-flex align-items-center">
                     <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
                 </div>
-                <div class="col-10">
-                    <div class="col-12">
-                        <p class="mb-2 ps-2">Bot name (INF1X)</p>
-                    </div>
-                    <div class="col-12 bg-primary text-white">
-                        <p class="p-2 ps-4">2,019 PT</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-2"></div>
-    </div>
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8">
-            <div class="row">
-                <div class="col-2 d-flex align-items-center">
-                    <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
-                </div>
-                <div class="col-10">
+                <div class="col-12 col-md-10">
                     <div class="col-12">
                         <p class="mb-2 ps-2">Bot name (INF1X)</p>
                     </div>
@@ -106,7 +107,7 @@
                 <div class="col-2 d-flex align-items-center">
                     <img class="w-100" src="../assets/img/battlebotlogo.png" alt="Picture of Robot">
                 </div>
-                <div class="col-10">
+                <div class="col-12 col-md-10">
                     <div class="col-12">
                         <p class="mb-2 ps-2">Bot name (INF1X)</p>
                     </div>
