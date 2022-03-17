8365d68f6138bf746b79295be01862c8a1e36ef7
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-03-17 13:46:42 +0100

    made the frontpage scale to browser size and make robot page variable

--- components/footer.php
+++ components/footer.php
@@ -1,24 +1,24 @@
 <footer class="footer">
     <div class="container text-uppercase">
         <div class="row text-center">
-            <div class="col-md-6 col-12 column">
+            <div class="col-lg-6 col-12 mb-3">
                 <h6>Dit evenement is gesponsord door:</h6>
-                <div class="rounded-circle">
+                <div class="rounded-circle mx-auto mt-4">
                     <img class="footerIMG" src="<?=($_SESSION['sort'] == 'page') ? '../assets/img/nhlstenden-logo.svg' : 'assets/img/nhlstenden-logo.svg'?>" alt="StendenLogo">
                 </div>
             </div>
-            <div class="col-md-3 col-12 column">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
                 <h6>Battlebot</h6>
-                <ul class="list-unstyled">
+                <ul class="list-unstyled mt-4">
                     <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? '../index.php' : 'index.php'?>">Voorpagina</a></li>
                     <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'robots.php' : 'pages/robots.php'?>">Robots</a></li>
                     <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'calendarpage.php' : 'pages/calendarpage.php'?>">Kalender</a></li>
                     <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'livestream.php' : 'pages/livestream.php'?>">Livestream</a></li>
                 </ul>
             </div>
-            <div class="col-md-3 col-12 column">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
                 <h6>Account</h6>
-                <ul class="list-unstyled">
+                <ul class="list-unstyled mt-4">
                     <?php if(isset($_SESSION['username'])) { ?>
                         <li><a class="text-decoration-none nav-link" href="<?= ($_SESSION['sort'] == 'page') ? 'profile.php' : 'pages/profile.php'?>">Profiel</a></li>
                     <?php } else { ?>
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 
 <head>
-    <?php 
-        include_once('components/head.html');
-        include_once('functions/function.php');
-        include_once('functions/database.php');
-        include_once('functions/formattedtime.php');
+    <?php
+    include_once('components/head.html');
+    include_once('functions/function.php');
+    include_once('functions/database.php');
+    include_once('functions/formattedtime.php');
     ?>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/footer.css">
@@ -20,12 +20,12 @@
     <section id="content">
         <div class="container">
             <div class="row py-5">
-                <div class="col-6">
+                <div class="col-lg-6 col-12">
                     <h1>Welkom bij Battlebots</h1>
                     <p>Battle Bots is een evenement georganiseerd door de eerste jaar studenten van het NHL Stenden Hogeschool te Emmen. Er doen 5 robots mee aan het event. Elke robot moet 5 spellen kunnen spelen. De robot die de meeste spellen wint, wint het evenement.</p>
                     <p>Het evenement vind plaats op donderdag 14 April.</p>
                 </div>
-                <div class="col-lg-6 text-center livestream">
+                <div class="col-lg-6 col-12 text-center livestream">
                     <a class="livestream" href="./pages/livestream.php">
                         <img class="img-fluid frontpageIMG" src="assets/img/photo1.png" alt="Battlebots">
                     </a>
@@ -41,24 +41,23 @@
                     <div class="col-12 text-center mb-4">
                         <h3 class="text-white">Ontmoet de Bots</h3>
                     </div>
-                    <div class="col-lg-12 d-flex justify-content-between">
+                    <div class="col-12 d-flex justify-content-between">
                         <?php
-
-                            $sql = "SELECT id, name, imagePath FROM bot";
-                            $dbResults = stmtExec($sql);
-                            $ids = $dbResults["id"];
-                            foreach($ids as $botId) {
-                                $id = $botId;
-                                $imgPath = $dbResults["imagePath"][$botId - 1];
-                                if($imgPath === "image.png") $imgPath = "assets/img/bots/BB_sawblaze-beauty.jpg";
-                                $name = $dbResults["name"][$botId - 1];
-                                echo "<div class='card'>
+                        $sql = "SELECT id, name, imagePath FROM bot";
+                        $dbResults = stmtExec($sql);
+                        $ids = $dbResults["id"];
+                        foreach ($ids as $botId) {
+                            $id = $botId;
+                            $imgPath = $dbResults["imagePath"][$botId - 1];
+                            if ($imgPath === "image.png") $imgPath = "assets/img/bots/BB_sawblaze-beauty.jpg";
+                            $name = $dbResults["name"][$botId - 1];
+                            echo "<div class='card'>
                                         <img src='$imgPath' class='img-fluid card-img-top' alt='$name'>
                                         <div class='card-body'>
-                                            <h5 class='card-title text-center'><a href='pages/robots.php?id=$id' class='stretched-link'>$name</a></h5>
+                                            <h5 class='card-title text-center'><a href='pages/robots.php?botName=$name' class='stretched-link'>$name</a></h5>
                                         </div>
                                     </div>";
-                            }
+                        }
                         ?>
                     </div>
                 </div>
@@ -74,20 +73,20 @@
                 </div>
                 <div class="col-lg-12 d-flex justify-content-center">
                     <?php
-                        $sql = "SELECT id, name, date, description FROM event";
-                        $eventResults = stmtExec($sql);
-                        $ids = $eventResults["id"];
+                    $sql = "SELECT id, name, date, description FROM event";
+                    $eventResults = stmtExec($sql);
+                    $ids = $eventResults["id"];
 
-                        foreach($ids as $eventId) {
-                            $name = $eventResults["name"][$eventId - 1];
-                            $eventDate = $eventResults["date"][$eventId - 1];
-                            // $date = $eventDate[0];
-                            
-                            $description = $eventResults["description"][$eventId - 1];
-                            echo "<div class='card mx-3 event'>
+                    foreach ($ids as $eventId) {
+                        $name = $eventResults["name"][$eventId - 1];
+                        $eventDate = $eventResults["date"][$eventId - 1];
+                        // $date = $eventDate[0];
+
+                        $description = $eventResults["description"][$eventId - 1];
+                        echo "<div class='card mx-3 event'>
                                     <div class='d-flex justify-content-left align-items-center'>
                                         <div>
-                                            <span class='calendarDate d-block'>".formatdate($eventDate)."</span>
+                                            <span class='calendarDate d-block'>" . formatdate($eventDate) . "</span>
                                             <span class='calendarTitle'>$name</span>
                                         </div>
                                     </div>
@@ -95,7 +94,7 @@
                                         <span class='calendarInfo mt-4'>$description</span>
                                     </div>
                                 </div>";
-                        }
+                    }
                     ?>
                 </div>
             </div>
