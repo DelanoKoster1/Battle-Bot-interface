9fc96b8e0b358d1c4324e66166011d268201a536
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-03-18 14:56:42 +0100

    did api reqest explained

--- websocket/src/server.js
+++ websocket/src/server.js
@@ -3,13 +3,20 @@ const express = require("express");
 const WebSocket = require("ws");
 const { isIPv4, Socket } = require("net");
 
-
 const app = express();
 
 app.use(express.json({ extended: false }));
 
 app.use("/client/chat", require("./routes/client/chat"));
 app.use("/robot/robot", require("./routes/robot/robot"));
+
+app.get("/api/robot-list", (req, res) => {
+  const robotId = {
+    robotId: ["macaddres1", "macaddres1", "macaddres1", "macaddres1"],
+  };
+  
+  res.json(robotId);
+});
 
 const port = 3001;
 const server = createServer(app);
