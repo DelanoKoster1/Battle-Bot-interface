2e01bbefb2da3abc6ece4839990ae373a09f6833
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2022-03-21 14:23:29 +0100

    added points display when hovering over bar

--- components/admin/points.php
+++ components/admin/points.php
@@ -6,27 +6,25 @@
                 <div class="col-4 d-flex align-items-center">
                     <span><?= $team ?></span>
                 </div>
-                <div class="col-8 d-flex justify-content-around">
-                <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team?>" value="25" autocomplete="off">
-                <label class="btn btn-secondary" for="<?=$team?>">25</label>
+                    <div class="col-8 d-flex justify-content-around">
+                    <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team?>" value="25" autocomplete="off">
+                    <label class="btn btn-secondary" for="<?=$team?>">25</label>
 
-                <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '1'?>" value="18" autocomplete="off">
-                <label class="btn btn-secondary" for="<?=$team . '1'?>">18</label>
+                    <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '1'?>" value="18" autocomplete="off">
+                    <label class="btn btn-secondary" for="<?=$team . '1'?>">18</label>
 
-                <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '2'?>" value="15" autocomplete="off">
-                <label class="btn btn-secondary" for="<?=$team . '2'?>">15</label>
+                    <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '2'?>" value="15" autocomplete="off">
+                    <label class="btn btn-secondary" for="<?=$team . '2'?>">15</label>
 
-                <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '3'?>" value="12" autocomplete="off">
-                <label class="btn btn-secondary" for="<?=$team . '3'?>">12</label>
+                    <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '3'?>" value="12" autocomplete="off">
+                    <label class="btn btn-secondary" for="<?=$team . '3'?>">12</label>
 
-                <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '4'?>" value="10" autocomplete="off">
-                <label class="btn btn-secondary" for="<?=$team . '4'?>">10</label>
-
-                
+                    <input type="radio" class="btn-check" name="<?= $teamId ?>" id="<?=$team . '4'?>" value="10" autocomplete="off">
+                    <label class="btn btn-secondary" for="<?=$team . '4'?>">10</label>     
                 </div>
             </div>
         <?php } ?>
-            <input type="submit" name="submitPoints" id="submitPoints" value="Punten toevoegen">
+            <input class="btn btn-danger mt-3" type="submit" name="submitPoints" id="submitPoints" value="Punten toevoegen">
         </form>
     </div>
     <div class="col-8">
