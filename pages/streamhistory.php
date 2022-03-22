c08f88396a5c30cb2e0385009f5a263a43a1c68a
Giovanni de Vries <giovanni.de.vries@hotmail.com>
2022-03-23 00:12:42 +0100

    make a prepare statment but isnt fully working yet

--- pages/streamhistory.php
+++ pages/streamhistory.php
@@ -21,11 +21,43 @@ include_once('../functions/function.php');
         <?php includeHeader('page'); ?>
     </section>
 
-    <div class="row">
-        <div class="col-12">Test</div>
-        <div class="col-12">Test</div>
-        <div class="col-12">Test</div>
-        <div class="col-12">Test</div>
+    <main id='streamhistory'>
+        <div class="container">
+            <div class="row mt-4">
+                
+            <?php
+              $query = "SELECT id, name, weburl,
+              FROM streamhistory 
+              WHERE date > now()
+              ORDER BY date ASC
+              limit 5";
+
+    $streamhistory = stmtExec($query);
+
+    if (!empty($streamhistory["id"])) {
+        $ids = $streamhistory["id"];
+
+        for ($i = 0; $i < count($ids); $i++) {
+            $name = $streamhistory["name"][$i];
+            $weburl = $streamhistory["weburl"][$i];
+
+            echo '
+            <div class="col-sm-3 mb-4">
+                <div class="card eventsCard">
+                    <div class="card-body">
+                        <span class="calendarTitle d-block text-capitalize">' . $name . '</span>
+                        <span class="calendarInfo mt-4 d-block">' . $weburl . '</span>
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+    }
+ ?>
+    </main>
+
+
+    </div>
     </div>
 
     <footer class="navbar">
