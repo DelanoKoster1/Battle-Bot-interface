a6aa29742f66ed77c3c914cdf730e5cacf3fc5a9
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-03-22 16:10:44 +0100

    deleted html from include pages

--- components/admin/director.php
+++ components/admin/director.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Director</title>
-</head>
-
-<body>
-    <div id="livestream" class="col-12">
-        <div class="ratio ratio-16x9">
-            <?= getLivestream(); ?>
-        </div>
+<div id="livestream" class="col-12">
+    <div class="ratio ratio-16x9">
+        <?= getLivestream(); ?>
     </div>
-</body>
-
-</html>
+</div>
--- components/admin/game.php
+++ components/admin/game.php
@@ -1,84 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <?php
-    include_once('../components/head.html');
-    include_once('../functions/function.php');
-    ?>
-
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/footer.css">
-    <link rel="stylesheet" href="../assets/css/gamepage.css">
-
-    <title>gamepage</title>
-</head>
-
-<body>
-    <?php include_once('../components/header.php'); ?>
-
-    <div class="container">
-        <h4 style="text-align: center; margin: 10px; padding: 10px;">Robots</h4>
-        <div class="row my-5">
-            <div class="col-lg-2 col-sm-4 col-6">
-                <div class="box bg-secondary d-flex justify-content-center start-button-all">
-                    <div class="row g-0 w-100 text-center">
-                        <div class="col-12 pt-1">
-                            <img src="../assets/img/playbutton.svg" alt="Logo of a playbutton">
-                        </div>
-                        <div class="col-12 position-relative">
-                            <div class="botName position-absolute w-100 bottom-0">
-                                <span class="start">Start!</span>
-                            </div>
+<link rel="stylesheet" href="../assets/css/gamepage.css">
+<?php
+include_once('../components/header.php');
+include_once('../functions/function.php');
+?>
+<div class="container">
+    <h4 style="text-align: center; margin: 10px; padding: 10px;">Robots</h4>
+    <div class="row my-5">
+        <div class="col-lg-2 col-sm-4 col-6">
+            <div class="box bg-secondary d-flex justify-content-center start-button-all">
+                <div class="row g-0 w-100 text-center">
+                    <div class="col-12 pt-1">
+                        <img src="../assets/img/playbutton.svg" alt="Logo of a playbutton">
+                    </div>
+                    <div class="col-12 position-relative">
+                        <div class="botName position-absolute w-100 bottom-0">
+                            <span class="start">Start!</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="doolhof" class="box bg-secondary d-flex justify-content-center game-card-all">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Doolhof</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="butler" class="box bg-secondary d-flex justify-content-center game-card-all">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Butler</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="race" class="box bg-secondary d-flex justify-content-center game-card-all">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Race</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
         </div>
-        <?php
-        $sql = "SELECT  bot.id,
+        <div class="col-lg-2 col-sm-4 col-6">
+            <div id="doolhof" class="box bg-secondary d-flex justify-content-center game-card-all">
+                <div class="row g-0 w-100 text-center">
+                    <div class="col-12 pt-1">
+                        <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                    </div>
+                    <div class="col-12 position-relative">
+                        <div class="botName position-absolute w-100 bottom-0">
+                            <span class="fw-bold">Doolhof</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-4 col-6">
+            <div id="butler" class="box bg-secondary d-flex justify-content-center game-card-all">
+                <div class="row g-0 w-100 text-center">
+                    <div class="col-12 pt-1">
+                        <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                    </div>
+                    <div class="col-12 position-relative">
+                        <div class="botName position-absolute w-100 bottom-0">
+                            <span class="fw-bold">Butler</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-4 col-6">
+            <div id="race" class="box bg-secondary d-flex justify-content-center game-card-all">
+                <div class="row g-0 w-100 text-center">
+                    <div class="col-12 pt-1">
+                        <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                    </div>
+                    <div class="col-12 position-relative">
+                        <div class="botName position-absolute w-100 bottom-0">
+                            <span class="fw-bold">Race</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    $sql = "SELECT  bot.id,
                         bot.name,
                         bot.imagePath,
                         team.name
@@ -86,85 +72,78 @@
                 INNER JOIN team
                         ON bot.id = team.botId
         ";
-        $dbResults = stmtExec($sql);
-        $ids = $dbResults["bot.id"];
-        foreach ($ids as $botId) {
-            $id = $botId;
-            $imgPath = $dbResults["bot.imagePath"][$botId - 1];
-            if ($imgPath === "image.png") $imgPath = "../assets/img/bot.svg";
-            $name = $dbResults["bot.name"][$botId - 1];
-            $teamName = $dbResults["team.name"][$botId - 1];
-        ?>
-            <div class="row my-5">
-                <h3 style="padding: 10px; margin: 10px;">Robot : <?=$name?> (<?=$teamName?>).</h3>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div class="box bg-secondary d-flex justify-content-center">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/bot.svg" alt="<?=$name?>">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span><?=$name?></span>
-                                </div>
+    $dbResults = stmtExec($sql);
+    $ids = $dbResults["bot.id"];
+    foreach ($ids as $botId) {
+        $id = $botId;
+        $imgPath = $dbResults["bot.imagePath"][$botId - 1];
+        if ($imgPath === "image.png") $imgPath = "../assets/img/bot.svg";
+        $name = $dbResults["bot.name"][$botId - 1];
+        $teamName = $dbResults["team.name"][$botId - 1];
+    ?>
+        <div class="row my-5">
+            <h3 style="padding: 10px; margin: 10px;">Robot : <?= $name ?> (<?= $teamName ?>).</h3>
+            <div class="col-lg-2 col-sm-4 col-6">
+                <div class="box bg-secondary d-flex justify-content-center">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 pt-1">
+                            <img src="../assets/img/bot.svg" alt="<?= $name ?>">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span><?= $name ?></span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="doolhof-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Doolhof</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="butler-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Butler</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-4 col-6">
-                    <div id="race-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
-                        <div class="row g-0 w-100 text-center">
-                            <div class="col-12 pt-1">
-                                <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
-                            </div>
-                            <div class="col-12 position-relative">
-                                <div class="botName position-absolute w-100 bottom-0">
-                                    <span class="fw-bold">Race</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
             </div>
-        <?php
-        }
-        ?>
-    </div>
+            <div class="col-lg-2 col-sm-4 col-6">
+                <div id="doolhof-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 pt-1">
+                            <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span class="fw-bold">Doolhof</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6">
+                <div id="butler-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 pt-1">
+                            <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span class="fw-bold">Butler</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-6">
+                <div id="race-<?= $id ?>" class="box bg-secondary d-flex justify-content-center game-card-single">
+                    <div class="row g-0 w-100 text-center">
+                        <div class="col-12 pt-1">
+                            <img src="../assets/img/joystick.svg" alt="Logo of a joystick">
+                        </div>
+                        <div class="col-12 position-relative">
+                            <div class="botName position-absolute w-100 bottom-0">
+                                <span class="fw-bold">Race</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-    <footer>
-        <?php include_once('../components/footer.php') ?>
-    </footer>
-    <script src="../assets/js/functions.js"></script>
-    <script src="../assets/js/game.js"></script>
-</body>
-
-</html>
+        </div>
+    <?php
+    }
+    ?>
+</div>
+<script src="../assets/js/functions.js"></script>
+<script src="../assets/js/game.js"></script>
