56c07604bc8379a817dea65dff928f28627803f6
Giovanni de Vries <giovanni.de.vries@hotmail.com>
2022-03-22 22:56:44 +0100

    added the base for the bootrap

--- pages/streamhistory.php
+++ pages/streamhistory.php
@@ -1,16 +1,19 @@
+<?php
+include_once('../functions/function.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php
     include_once('../components/head.html');
-    include_once('../functions/function.php');
     ?>
 
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/robots.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
-    <title>Bot Page</title>
+
+    <title>Geschiedenis</title>
 </head>
 
 <body>
@@ -18,26 +21,16 @@
         <?php includeHeader('page'); ?>
     </section>
 
-    <main id='events'>
-        <div class='containter py-4'>
-            <div class="row">
-                <div class="col-12 mb-2 text-center">
-                    <h3>Stream geschiedenis</h3>
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-12">Test</div>
+        <div class="col-12">Test</div>
+        <div class="col-12">Test</div>
+        <div class="col-12">Test</div>
+    </div>
 
-            <div class="row m-auto eventShowBox">
-                <?php showEvents(); ?>
-            </div>
-        </div>
-    </main>
-
-
-</body>
-
-<footer>
-    <?php include_once('../components/footer.php') ?>
-</footer>
+    <footer class="navbar">
+        <?php include_once('../components/footer.php') ?>
+    </footer>
 </body>
 
 </html>
