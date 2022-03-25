35f5bd02399f642385f737e8aa590c607a147f4d
Clibzz <chriscrossje2001@gmail.com>
2022-03-25 14:41:02 +0100

    Fix for admin.php & some minor other changes

--- pages/admin.php
+++ pages/admin.php
@@ -29,7 +29,7 @@ switch (true) {
         break;
 
     case isset($_GET['director']);
-        $headerTitle = 'Ressigeur pagina';
+        $headerTitle = 'Regisseur pagina';
         $content = "../components/admin/director.php";
         break;
     case isset($_GET['bot']);
@@ -37,11 +37,11 @@ switch (true) {
         $content = "../components/admin/bot.php";
         break;
     case isset($_GET['addRobotToEvent']);
-        $headerTitle = 'Robot aan event toevoegen';
+        $headerTitle = 'Robot aan evenement toevoegen';
         $content = "../components/admin/addRobotToEvent.php";
 
     case isset($_GET['addTeamToEvent']);
-        $headerTitle = 'Team aan event toevoegen';
+        $headerTitle = 'Team aan evenement toevoegen';
         $content = "../components/admin/addTeamToEvent.php";
         break;
     case isset($_GET['startEvent']);
@@ -54,7 +54,7 @@ switch (true) {
         break;
 
     default:
-        $headerTitle = 'Event toevoegen';
+        $headerTitle = 'Evenement toevoegen';
         $content = "../components/admin/event.php";
         break;
 }
@@ -67,25 +67,25 @@ function checkEventFields($eventDate, $eventName, $eventDescription, $eventType)
     $error = array();
 
     if (!$eventDate && empty($eventDate)) {
-        $error[] = 'Event datum mag niet leeg zijn!';
+        $error[] = 'De evenement datum mag niet leeg zijn!';
     } else {
         if (!checkValidDate($eventDate)) {
-            $error[] = 'Event datum is ongeldig!';
+            $error[] = 'De evenement datum is ongeldig!';
         }
     }
     if (!$eventDescription && empty($eventDescription)) {
-        $error[] = 'Event omschrijving mag niet leeg zijn!';
+        $error[] = 'De evenement omschrijving mag niet leeg zijn!';
     }
     if (!$eventName && empty($eventName)) {
-        $error[] = 'Event naam mag niet leeg zijn!';
+        $error[] = 'De evenement naam mag niet leeg zijn!';
     }
     if (!$eventType && empty($eventType)) {
-        $error[] = 'Event type mag niet leeg zijn!';
+        $error[] = 'Het evenement type mag niet leeg zijn!';
     } else {
         if ($eventType == 'public' || $eventType == 'private') {
             //Do nothing
         } else {
-            $error[] = 'Event type klopt niet';
+            $error[] = 'Het evenement type klopt niet';
         }
     }
 
@@ -140,7 +140,7 @@ if (isset($_POST['event'])) {
         }
 
         //Set succes message
-        $_SESSION['succes'] = 'Event toegevoegd!';
+        $_SESSION['succes'] = 'Het evenement is succesvol toegevoegd!';
 
         //Send user to index.php
         header('location: admin.php');
@@ -255,7 +255,7 @@ if (isset($_GET['points']) && isset($_GET['eventId'])) {
                 $assignedPoints = $_POST[$radioTeamId];
                 $sql = "UPDATE `team-event` SET points = ? WHERE teamId = ?";
             } else {
-                $error[] = "Vul een getal in";
+                $error[] = "Vul een getal in!";
             }
         }
 
@@ -325,7 +325,7 @@ if (isset($_POST['selectedEvent'])) {
 
         //Check if no result has been found
         if (is_array($results) && count($results) > 0) {
-            $error[] = 'Dit team is al toegevoegd aan dit event';
+            $error[] = 'Dit team is al toegevoegd aan dit evenement!';
             $_SESSION['ERROR_MESSAGE'] = $error;
             unset($_SESSION['selectedTeam']);
             header('location: admin.php?addTeamToEvent');
@@ -342,7 +342,7 @@ if (isset($_POST['selectedEvent'])) {
             unset($_SESSION['selectedTeam']);
 
             //Set succes message
-            $_SESSION['succes'] = 'Team aan event toegevoegd!';
+            $_SESSION['succes'] = 'Het team is succesvol aan het evenement toegevoegd!';
 
             //Send user to admin.php?addTeamToEvent
             header('location: admin.php?addTeamToEvent');
@@ -379,7 +379,7 @@ if (isset($_POST['stopEvent'])) {
 
 <head>
     <?php
-    include_once('../components/functions/function.php');
+    include_once('../functions/function.php');
     includeHead('page'); 
     ?>
     <link href="../assets/img//logo/logo.ico" rel="icon" type="image/x-icon">
@@ -401,7 +401,7 @@ if (isset($_POST['stopEvent'])) {
                 <nav class="navbar">
                     <ul class="nav w-100 nav-fill pt-2">
                         <li class="nav-item w-100">
-                            <a class="nav-link text-white" href="admin.php">Event toevoegen</a>
+                            <a class="nav-link text-white" href="admin.php">Evenement toevoegen</a>
                         </li>
                         <li class="nav-item w-100">
                             <a class="nav-link text-white" href="admin.php?poll">Poll toevoegen</a>
@@ -419,10 +419,10 @@ if (isset($_POST['stopEvent'])) {
                             <a class="nav-link text-white" href="admin.php?bot">Bot toevoegen</a>
                         </li>
                         <li class="nav-item w-100">
-                            <a class="nav-link text-white" href="admin.php?addTeamToEvent">Team toevoegen aan event</a>
+                            <a class="nav-link text-white" href="admin.php?addTeamToEvent">Team toevoegen aan evenement</a>
                         </li>
                         <li class="nav-item w-100">
-                            <a class="nav-link text-white" href="admin.php?startEvent">Event activeren</a>
+                            <a class="nav-link text-white" href="admin.php?startEvent">Evenement starten</a>
                         </li>
                         <li class="nav-item w-100">
                             <a class="nav-link text-white" href="admin.php?edit">Informatie wijzigen</a>
