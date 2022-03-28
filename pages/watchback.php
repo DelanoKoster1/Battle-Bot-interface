714a5415418fc0f67a84c19f541e8169ee09f181
Giovanni de Vries <giovanni.de.vries@hotmail.com>
2022-03-28 15:07:48 +0200

    added css so the video will be the full width of the pages

--- pages/watchback.php
+++ pages/watchback.php
@@ -12,6 +12,7 @@ include_once('../functions/function.php');
 
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" href="../assets/css/watchback.css">
 
     <title>Terugkijken</title>
 </head>
@@ -36,7 +37,7 @@ include_once('../functions/function.php');
     $historyResults = stmtExec($query);
     ?>
 
-    <video width="840" height="600" autoplay muted controls>
+    <video width="250"  autoplay muted controls id="VideoPlayback">
         <source src="/Project Battle Bot/Battle-Bot-interface/assets/video/Purple_Disco_Machine,_Sophie_and_the_Giants_-_In_The_Dark.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
