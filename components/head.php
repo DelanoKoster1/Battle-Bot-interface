cc44adbb7c7670eaeacf3381f709fc5209e6e1bd
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2022-03-29 17:48:01 +0200

    fixed tooltip issue

--- components/head.php
+++ components/head.php
@@ -7,6 +7,7 @@
 <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,700&family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
 <link href="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/css/bootstrap.min.css' : 'assets/bootstrap-5.0.2/css/bootstrap.min.css' ?>" rel="stylesheet">
 <script src="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/js/bootstrap.min.js' : 'assets/bootstrap-5.0.2/js/bootstrap.min.js' ?>"></script>
+<script src="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js' : 'assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js' ?>"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
