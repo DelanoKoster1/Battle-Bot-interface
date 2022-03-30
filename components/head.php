64cac6ee47ed0e56e00d0b178083960b5eebbb56
ItzJoris03 <jorishummel03@gmail.com>
2022-03-30 14:57:06 +0200

    installed bootstrap

--- components/head.php
+++ components/head.php
@@ -5,7 +5,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,700&family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
-<link href="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/css/bootstrap.min.css' : 'assets/bootstrap-5.0.2/css/bootstrap.min.css' ?>" rel="stylesheet">
-<script src="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/js/bootstrap.min.js' : 'assets/bootstrap-5.0.2/js/bootstrap.min.js' ?>"></script>
-<script src="<?= ($_SESSION['sort'] == 'page') ? '../assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js' : 'assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js' ?>"></script>
+<link href="<?= ($_SESSION['sort'] == 'page') ? '../node_modules/bootstrap/dist/css/bootstrap.min.css' : 'node_modules/bootstrap/dist/css/bootstrap.min.css' ?>" rel="stylesheet">
+<script src="<?= ($_SESSION['sort'] == 'page') ? '../node_modules/bootstrap/dist/js/bootstrap.min.js' : 'node_modules/bootstrap/dist/js/bootstrap.min.js' ?>"></script>
+<script src="<?= ($_SESSION['sort'] == 'page') ? '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js' : 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js' ?>"></script>
 <script src="<?= ($_SESSION['sort'] == 'page') ? '../node_modules/jquery/dist/jquery.min.js' : 'node_modules/jquery/dist/jquery.min.js' ?>"></script>
