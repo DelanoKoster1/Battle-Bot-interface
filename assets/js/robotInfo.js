4b4ed3c7b1bcf377983ec38254f1b1aeb2be77fe
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2022-04-05 16:12:13 +0200

    updated port

--- assets/js/robotInfo.js
+++ assets/js/robotInfo.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket(`ws://${getDomainName()}:3003/websocket/robot`);
+const ws = new WebSocket(`ws://${getDomainName()}:33003/websocket/robot`);
 
 ws.addEventListener("open", () => {
     ws.send(
